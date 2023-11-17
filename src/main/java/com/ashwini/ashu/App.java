acb6bb355468bc2260dd11f1bc10e0a15c80f5a4
Sangamesh Basarakod <basarakod@Sangameshs-MacBook-Pro.local>
2023-11-17 15:32:16 -0600

    Testing the stage

--- src/main/java/com/ashwini/ashu/App.java
+++ src/main/java/com/ashwini/ashu/App.java
@@ -4,10 +4,8 @@ package com.ashwini.ashu;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! hello San" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World! San Ashu");
+	}
 }
